1cb03d92a1d11748e64036cf29b9521c7fc3fcb7
Karoliina Salenius <58782294+karoliinas@users.noreply.github.com>
2025-07-14 12:47:14 +0300

    Create deepvariant.sh

---
+++ variant_calling/deepvariant.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# GEMMA germline variant calling on bwa-mem aligned bam -files using deepvariant-1.6.1
+# optimized for 56 cpu VM
+# Change the number of cpus and parallel calls to suit the available resources
+#
+# Outputs all logs and variant statistics to dir named after bam -file prefix (sampleid)
+#
+#
+#
+export cpus=12
+export n_parallel_calls=4
+export genome_ref="/mnt/gemma/bin/resources/homo_sapiens/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+export docker_mnt="/mnt:/mnt"
+export regions="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM"
+export parallel_run_file="run_DV.cmd"
+export wd=$(pwd)
+for file in *.bam;
+do
+    export sample=$(echo $file | awk -F'.' '{print $1}')
+    echo "Call variants for $sample"
+    export reads=$wd"/"$file
+    export vcf=$wd"/"$sample".vcf.gz"
+    export gvcf=$wd"/"$sample".g.vcf.gz"
+    export intermediate="${wd}/${sample}_intermediate_files"
+    export log="${wd}/${sample}_logs"
+    echo $reads $vcf $gvcf
+    echo $log $intermediate
+    echo "# $sample" >> $parallel_run_file
+    echo "docker run -v $docker_mnt google/deepvariant /opt/deepvariant/bin/run_deepvariant --model_type=WGS --ref=$genome_ref --reads=$reads --sample_name=$sample --output_vcf=$vcf --output_gvcf=$gvcf --intermediate_results_dir=$intermediate --regions '$regions' --num_shards=$cpus --logging_dir=$log" >> $parallel_run_file
+    echo "" >> $parallel_run_file
+done
+
+
+parallel2_95.py --job-name "deepvaraint" -n $n_parallel_calls -f $parallel_run_file > $logfile 2>&1
